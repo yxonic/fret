990e06c938688dcc6e4a08e020fad02be85916b5
Yu Yin <yxonic@gmail.com>
2019-02-28 10:57:21 +0800

    prepare for 0.2.0 beta release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='fret',
-    version='0.1.5.post5',
+    version='0.2.0.beta1',
     url='https://github.com/yxonic/fret',
     license='MIT',
     author='yxonic',
