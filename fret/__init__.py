fe1056da445c5e561044c8721c31b9cb6a4ba9c0
Yu Yin <yxonic@gmail.com>
2020-11-21 22:13:09 +0800

    expose fret.use

--- fret/__init__.py
+++ fret/__init__.py
@@ -1,10 +1,10 @@
-from .common import configurable, command, argspec
+from .common import configurable, command, argspec, use
 from .workspace import Workspace as workspace
 from .util import nonbreak, stateful
 
 __all__ = [
     'configurable', 'command', 'argspec', 'nonbreak', 'stateful',
-    'workspace'
+    'workspace', 'use',
 ]
 
 __version__ = '0.3.2'
